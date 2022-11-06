d9173884bbe71001d117ecc4cadeae86f9072da7
yiwengao <yiwen.gao@columbia.edu>
2022-11-06 08:29:39 -0800

    Remove comments

--- xmtp.js
+++ xmtp.js
@@ -2,7 +2,7 @@ const { Client } = require('@xmtp/xmtp-js');
 const { Wallet } = require('ethers');
 
 const sendXMTPMessage = async (recipientAddress, message) => {
-    const botWallet = Wallet.createRandom();  // TODO make non-random address
+    const botWallet = Wallet.createRandom();
     const xmtp = await Client.create(botWallet);
     const conversation = await xmtp.conversations.newConversation(recipientAddress);
     await conversation.send(message);
